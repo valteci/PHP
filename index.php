2a2fc3c160167052ae6f5ef47a164c78882abca9
valteci <valtecijunior@gmail.com>
2024-02-24 15:47:34 -0300

    teste

--- index.php
+++ index.php
@@ -2,30 +2,19 @@
 <html>
     <head>
         <title>Bet.me</title>
-        <meta charset="utf-8">        
+        <meta charset="utf-8">
     </head>
-    <body>   
-        <h1>     
-            <?php
-$deslocamento = 1;
-$inicio = 3;
+    <body>
+        <h1>
+            <?php                
+                
+                $a = array();
+                $a[1] = "asdf";
+                $a[2] = "dvbdf";
 
-switch (true) {
-    case $inicio - $deslocamento === 1:
-        print "A";
-        break;
-    case $inicio - $deslocamento === 2:
-        print "B";
-        break;
-    case $inicio - $deslocamento === 3:
-        print "C";
-        break;
-    case $inicio - $deslocamento === 4:
-        print "D";
-        break;
-}
+                print_r($a);
+
             ?>
         </h1>
-            
     </body>
 </html>
